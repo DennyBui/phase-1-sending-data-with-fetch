e58ec65df8e9464a67052885783f81a6fd89adf7
DennyBui <dbuidmd2018@gmail.com>
2023-04-21 09:07:09 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = { name, email };
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    
+    return fetch('http://localhost:3000/users', configObj)
+      .then(res => res.json())
+      .then(data => {
+        const newId = data.id;
+        document.body.append(newId);
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
